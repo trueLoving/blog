381738954e5fa4d4528c5ff28e46962e892de961
star sky <1176789241@qq.com>
2020-05-21 13:43:34 +0800

    博客主页更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,4 @@
 module.exports = {
-    // title: "Re_Star's Blog",//网站名称
-    // description: '一个轮子狂魔的学习笔记     (～￣(OO)￣)ブ',//网站描述
     title: "Re_Star's Blog",
     base: '/blog/',
     theme: 'reco',
@@ -16,11 +14,10 @@ module.exports = {
                 text: 'Tag'      // 默认文案 “标签”
             }
         },
-        // sidebar: 'auto',//在所有页面中启用自动生成侧栏
         author: 'Re_Star',
         authorAvatar: '/avatar.png',
-        type: 'blog',
-        logo:'/logo.png',
+        type: 'HomePage',
+        logo: '/logo.png',
         nav: [
             { text: 'TimeLine', link: '/timeline/', icon: 'reco-date' },
             { text: 'Github', link: 'https://github.com/trueLoving', icon: 'reco-github' }
--- docs/README.md
+++ docs/README.md
@@ -3,7 +3,8 @@ home: true
 description: '一个轮子狂魔的学习笔记     (～￣(OO)￣)ブ'
 bgImage: '/bg.jpg'
 heroText: 'Re Star'
+navbar: false
 bgImageStyle: {
-  height: '40vh'
+  height: '100vh'
 }
 ---
